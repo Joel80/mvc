a12670298a4f16f545e4a101360f02fc616ad37a
Joel Löfgren <jdtlofgren@gmail.com>
2021-04-06 14:14:14 +0100

    Added info about course in the webpage footer

--- view/footer.php
+++ view/footer.php
@@ -5,7 +5,7 @@ use function Mos\Functions\url;
 ?>
 </main>
 <footer>
-    <p>This is the footer.</p>
+Denna webbplats är skapad inom ramen för kursen <a href="https://dbwebb.se/kurser/mvc-v1">mvc</a> vid Blekinge Tekniska Högskola.</p>
     <script type="text/javascript" src=<?= url("/js/main.js")?>></script>
 </footer>
 </body>
